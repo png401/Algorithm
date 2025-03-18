ac943a0217b7ed6a9f6a4bccdb9408e1580e73bd
png401 <png401@gmail.com>
2025-03-18 19:48:58 +0900

    [Bronze III] Title: 배수와 약수, Time: 172 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+       int first, second;
+        
+        while(true){
+        	  first = input.nextInt();
+              second = input.nextInt();
+        
+              if((first==0)&&(second==0)) {
+            	  break;
+              }
+              
+        if(second%first == 0){
+            System.out.println("factor");
+            
+        }
+        else if(first%second == 0){
+            System.out.println("multiple");
+        }
+        //second%first == 0 -> factor
+        //first%second == 0 -> multiple
+        else
+            System.out.println("neither");
+        }  
+        
+        input.close();
+        
+    }
+    
+}
