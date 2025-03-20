5b3ede8e34a320a50827f0a8260e78c7d811536a
png401 <png401@gmail.com>
2025-03-20 19:29:35 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int score = sc.nextInt();
-        
-        if(score>=90){
-            System.out.println('A');
-        } else if(score>=80){
-            System.out.println('B');
-        } else if(score>=70){
-            System.out.println("C");
-        } else if(score>=60){
-            System.out.println("D");
-        } else
-            System.out.println("F");
-        
-        sc.close();
-    }
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	int score = input.nextInt();
+	
+	switch(score/10) {
+		case 10:
+		case 9:
+			System.out.println("A");
+			break;
+		case 8:
+			System.out.println("B");
+			break;
+		case 7:
+			System.out.println("C");
+			break;
+		case 6:
+			System.out.println("D");
+			break;
+		default:
+			System.out.println("F");
+			break;
+	}
+	
+	}
 }
