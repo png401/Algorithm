763fe6ad04056612ddf5859c011fa04367c8844e
png401 <png401@gmail.com>
2025-04-04 23:47:23 +0900

    [Bronze III] Title: 공 넣기, Time: 208 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // 바구니 개수
-        int M = sc.nextInt(); // 공을 넣는 횟수
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int [] arr = new int [N];
+		
+		for(int r=0; r<M; r++) {
+		int i=sc.nextInt();
+		int j=sc.nextInt();
+		int k=sc.nextInt();
+		
+		for(int r1=(i-1); r1<=j-1; r1++) {
+			arr[r1] = k;
+		}
+		
+		}
+		
+		for(int r=0; r<N; r++) {
+			System.out.print(arr[r]+" ");
+		}
+		
+	}
 
-        int[] baskets = new int[N]; // 바구니 배열 (0으로 초기화됨)
-
-        for (int m = 0; m < M; m++) {
-            int i = sc.nextInt(); // 시작 바구니
-            int j = sc.nextInt(); // 끝 바구니
-            int k = sc.nextInt(); // 넣을 공 번호
-
-            for (int b = i - 1; b <= j - 1; b++) {
-                baskets[b] = k; // 공을 넣는다 (덮어쓰기)
-            }
-        }
-
-        // 결과 출력
-        for (int ball : baskets) {
-            System.out.print(ball + " ");
-        }
-    }
 }
