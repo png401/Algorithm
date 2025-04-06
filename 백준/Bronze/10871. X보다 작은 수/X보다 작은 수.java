ae634186a3eb47bc38a84f7571e7e9f816f204d2
png401 <png401@gmail.com>
2025-04-06 16:11:04 +0900

    [Bronze V] Title: X보다 작은 수, Time: 492 ms, Memory: 31600 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class Main{
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
+	int N = sc.nextInt();
+	int X = sc.nextInt();
+	
+	int [] ar = new int [N];
+	int [] final_ar = new int [N];
+	
+	for(int i=0; i<N; i++) {
+		ar[i] = sc.nextInt();
 		
-		int N = input.nextInt();
-		int X = input.nextInt();
-		
-		int number [] = new int [N];
-		
-		for(int i=0; i<N; i++) {
-			number[i] = input.nextInt();
+		if(ar[i] < X) {
+			final_ar[i] = ar[i];
 		}
-		
-		for(int i=0; i<N; i++) {
-			if(number[i]<X) {
-				System.out.print(number[i]+" ");
-			}
-		}
-		
 	}
-
+	
+	for(int j=0; j<N; j++) {
+		if(final_ar[j]!=0)
+		System.out.print(final_ar[j]+" ");
+	}
+}
 }
