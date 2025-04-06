99bacd85855da92536d90d94b8436f7cc369b516
png401 <png401@gmail.com>
2025-04-06 15:52:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 284 ms, Memory: 18380 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		
-		int floor = input.nextInt();
-		
-		for(int i=1; i<=floor; i++) {
-			for(int j=1; j<=floor-i; j++) {
-				System.out.print(" ");
-			}
-			for(int k=1; k<=i; k++) {
-				System.out.print("*");
-			}
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int count = input.nextInt();
+        
+        for(int i=1; i<=count; i++){
+            for(int j=1; j<=(count-i); j++){
+                System.out.print(" ");
+                
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
             System.out.println();
-		}
-		
-		
-	}
-
+        }
+        
+        input.close();
+    }
+    
+    
 }
