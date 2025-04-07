af7ff7496b3578386a22494169ebb43703486730
png401 <png401@gmail.com>
2025-04-07 23:36:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,40 +1,35 @@
 import java.util.Scanner;
 
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        
-        int num = A*B*C;
-        
-        int arr [] = new int [10];
-        
-        /*
-        int one = num%10; //일의 자리수
-        int two = (num/10)%10; //십의 자리수
-        int three =((num/10)/10)%10;//백의 자리수
-        */
-       
-        
-        while (num > 0) {
-        	int t = num % 10;
-        	
-        	arr[t]++;
-        	num /= 10;
-        	
-        }
-        
-        for(int i=0; i<10; i++) {
-        System.out.println(arr[i]);
-        }
-       
-        
-        
-        
-        sc.close();
-    }
+public class Main{
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int [] jari = new int [10];
+	
+	int A = sc.nextInt();
+	int B = sc.nextInt();
+	int C = sc.nextInt();
+	
+	int result = A*B*C;
+	int ah=0;
+	while(true) {
+		
+		ah = result % 10;
+		jari [ah] ++;
+		result = result /10;
+		
+		if(result ==0) break;
+		
+	}
+	
+	for(int i=0; i<jari.length; i++) {
+		System.out.println(jari[i]);
+	}
+	
+	//result % 10; //1의 자리 수
+	
+	
+	
+	}
 }
