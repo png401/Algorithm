ad089ce805af5b59f7258bb5da648b2fd2088b2e
png401 <png401@gmail.com>
2025-04-07 23:05:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 1580 ms, Memory: 319312 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt(); // 정수 개수 입력
-        int array [] = new int [N];
-        
-        //배열 입력
-        for(int i=0; i<N; i++){
-            array [i] = sc.nextInt();
-        }
-        
-        //최대, 최소값 초기화
-        int min = array[0];
-        int max = array[0];
-        
-        //루프 돌면서 갱신
-        for(int i=1; i<N; i++){
-            if(array[i]<min) min=array[i];
-            if(array[i]>max) max=array[i];
-        }
-        
-        System.out.println(min + " " + max);
-        sc.close();
-    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int [] ar = new int [N];
+		//int max=-1000001,min=1000000;
+		
+		for(int i=0; i<N; i++) {
+			ar[i] = sc.nextInt();
+		}
+		
+		int max = ar[0];
+		int min = ar[0];
+		
+		for(int i=0; i<N; i++) {
+			if(ar[i]>max) max = ar[i];
+			
+			if(ar[i]<min) min = ar[i];
+		}
+		
+		System.out.print(min + " "+max);
+	}
+
 }
