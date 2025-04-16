98e947876173d229494b14ba15c119c6ef927594
png401 <png401@gmail.com>
2025-04-16 14:51:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.28 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,31 +2,34 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+        int[] answer = {};
+        //이전 값 기억
+        //이전 값과 다를 때만 리스트에 추가
+        //이전 값 업데이트
+        ArrayList <Integer> list = new ArrayList<>();
         
-      
-            //이전 숫자 기억하기
-            //다르면 그 숫자 저장하기
-            ArrayList <Integer> result = new ArrayList <>();
-            
-            int temp = -1;
-            
-            for(int num : arr){
-                if(num != temp){
-                result.add(num);
-                temp = num;
-            }
+        int before = -1;
+        for(int num : arr){
+            if(num != before){
+                list.add(num);
+                before = num;
             }
             
-        int[] answer = new int [result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i);
         }
         
         
         
         
+        //ArrayList<Integer> -> int [] 로 변환
+        answer = new int [list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        //System.out.println("Hello Java");
+        System.out.println("Hello Java");
 
         return answer;
     }
