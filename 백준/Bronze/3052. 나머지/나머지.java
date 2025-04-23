69d273681b9be104ba34317a3a5f50632d81812a
png401 <png401@gmail.com>
2025-04-23 10:14:39 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,34 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int [] arr = new int [10];
-		int [] arr1 = new int [42];
-		int count=0;
-		//배열 입력
-		for(int i=0; i<10; i++) {
-			arr[i] = sc.nextInt();
-			arr1[arr[i]%42] = 1;  
-		}
-		
-		
-
-		/*for(int i=0; i<41; i++)
-			System.out.print(arr1[i] +" "); */
-		
-		//42로 나눈 나머지값들에 대한 배열
-		for(int i=0; i<42; i++) {
-	
-			if(arr1[i]==1) count++;
-		}
-		System.out.println(count);
-		
-
-		sc.close();
-	}
-
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int [42];
+        //수 10개 입력, 이를 42로 나눈 나머지
+        //입력받을 때마다 42로 나누었을 때의 나머지를 배열에 입력
+        //(배열크기 42로 하면 나머지 0~41이 표현되잖아..?)
+        //
+        //예) 1 1 1 1 1 2 2 2 2 2
+        
+        for(int i=0; i<10; i++) {
+        	int input = sc.nextInt();
+        	int num = input%42;
+        	arr[num]++;
+        }
+        
+        int count=0;
+        
+        for(int j=0; j<arr.length; j++) {
+        	if(arr[j] != 0) {
+        		count++;
+        	}
+        }
+        System.out.println(count);
+    }
+    
 }
