db897648c6fad069e133837e67e952608a1ad96a
png401 <png401@gmail.com>
2025-04-25 09:45:29 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 476 ms, Memory: 36528 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,47 +1,53 @@
 import java.util.Scanner;
 
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		int [][] A = new int [N][M];
-		int [][] B = new int [N][M];
-		int [][] result = new int [N][M];
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				A[i][j] = sc.nextInt();
-			}
-		}
-		
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				B[i][j] = sc.nextInt();
-			}
-		}
-		
-		
-		
-		for(int b=0; b<N; b++) {
-			for(int a=0; a<M; a++) {
-				result[b][a] = A[b][a] + B[b][a];
-			}
-		}
-		
-		for(int c=0; c<N; c++) {
-			for(int d=0; d<M; d++) {
-				System.out.print(result[c][d]+" ");
-			}
-			System.out.println();
-		}
-		
-		
-	}
-
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int [][] A = fillMatrix(sc, N, M);
+        int [][] B = fillMatrix(sc, N, M);
+        
+        
+        //이번엔 리턴값 있는 함수. not void!
+        int [][] result = sumMatrix(A, B);
+        showMatrix(result);
+    }
+    //행렬 입력받는 함수
+     public static int[][] fillMatrix (Scanner sc, int N, int M) {
+    	 int [][] ar = new int[N][M];
+    	 
+    	 for(int i=0; i<ar.length; i++) {
+    		 for(int j=0; j<ar[0].length; j++) {
+    			 ar[i][j] = sc.nextInt();
+    		 }
+    	 }
+    	 return ar;
+     }
+     
+     public static int[][] sumMatrix(int[][] a, int[][]b){
+    	 int rows = a.length;
+    	 int cols = a[0].length;
+    	 int [][] total = new int [rows][cols];
+    	 for(int i=0; i<a.length; i++) {
+    		 for(int j=0; j<a[0].length; j++) {
+				total[i][j] = a[i][j]+b[i][j];
+    		 }
+    	 }
+		return total;
+     }
+     
+     //출력은 걍 void. 따로 리턴할 게 없고 출력하는 거니까
+     //그니까 result 배열 받아서 그걸 그대로 출력..
+     public static void showMatrix(int[][] a){
+    	 for(int i=0; i<a.length; i++) {
+    		 for(int j=0; j<a[0].length; j++) {
+    			 System.out.print(a[i][j]+" ");
+    		 }System.out.println();
+    	 }
+     }
+    
 }
