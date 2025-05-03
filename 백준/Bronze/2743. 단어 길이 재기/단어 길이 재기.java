06a506bb7e1ee5af2d29d42bb324d18c2efb19db
png401 <png401@gmail.com>
2025-05-03 23:59:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();  // 단어 입력 받기
+        System.out.println(word.length());  // 문자열 길이 출력
+    }
+}
