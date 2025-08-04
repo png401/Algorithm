86c568ee08fcf49e68c7c28fb3c00296de2bbd84
png401 <png401@gmail.com>
2025-08-04 17:11:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 1524 ms, Memory: 317664 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,27 +1,35 @@
 import java.util.Scanner;
-public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int [] ar = new int [N];
-		//int max=-1000001,min=1000000;
-		
-		for(int i=0; i<N; i++) {
-			ar[i] = sc.nextInt();
-		}
-		
-		int max = ar[0];
-		int min = ar[0];
-		
-		for(int i=0; i<N; i++) {
-			if(ar[i]>max) max = ar[i];
-			
-			if(ar[i]<min) min = ar[i];
-		}
-		
-		System.out.print(min + " "+max);
-	}
-
+//만약 for문 하나 안에서 모든 걸 하기 어렵다면 그냥 조각조각 단계나눠서 하기
+//입력for문, 초기화따로, 출력따로
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        //처음 입력된 걸 max,min값으로 설정하려면 어케야 하지...?
+        int[] ar = new int [N];
+        
+        //배열에 값들 입력 따로, max 산출 따로, 출력 따로 하기
+        for(int i=0; i<N; i++) {
+        	ar[i] = sc.nextInt();
+        	//이렇게 하면... 기껏 max값에 최대값 넣어뒀는데 새로운 입력값들로 초기화되지 않나..?
+        	/*max=num;
+        	min=num;
+        	if(num>max) {
+        		max=num;
+        	}*/
+        }
+        int max= ar[0];
+        int min=ar[0];
+        //max,min값
+        for(int i=0; i<N; i++) {
+        	//이게 초기화한 거 아님?
+        	if(ar[i]>max) max=ar[i];
+        	if(ar[i]<min) min=ar[i];
+        }
+        
+        System.out.println(min +" "+ max);
+        
+     }
+    
 }
