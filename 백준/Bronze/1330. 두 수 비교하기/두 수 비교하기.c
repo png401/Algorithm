0a363918ebaa53d99c2de81dcbe0eb223c5650dc
png401 <png401@gmail.com>
2025-09-08 18:30:03 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if(a>b){
+        printf(">");
+}
+
+else if(b>a){
+printf("<");
+}
+    
+    else {
+printf("==");
+
+}
+    
+return 0;
+
+}
