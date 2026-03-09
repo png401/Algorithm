9e99328a7d653fce5f2d4ca568c0fbbe30ce8280
png401 <png401@gmail.com>
2026-03-09 23:54:42 +0900

    [Silver IV] Title: 스택, Time: 208 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    stack<int> s;
+
+    for(int i=0; i<N; i++){
+        string cmd;
+        cin>>cmd;
+
+        if (cmd == "push"){
+            int x;
+            cin>>x;
+            s.push(x);
+        }
+
+        else if (cmd == "pop") {
+            if (s.empty()) {
+                cout<<-1<<"\n";
+            }
+            else {
+                cout<<s.top()<<"\n";
+                s.pop();
+            }
+        }
+
+        else if (cmd == "size") {
+            cout<<s.size()<<"\n";
+        }
+
+        else if (cmd == "empty") {
+            cout<<s.empty()<<"\n";
+        }
+
+        else if (cmd == "top") {
+            if (s.empty()) cout<<-1<<"\n";
+            else {
+                cout<<s.top()<<"\n";
+            }
+        }
+
+    }
+
+}
