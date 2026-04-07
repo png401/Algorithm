b506a7473c2293177af7d0755ab36cb0fd0cbd96
Namgyu Park <png401@gmail.com>
2026-04-07 14:10:31 +0900

    [Gold V] Title: 물병, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.cc
+++ 백준/Gold/1052. 물병/물병.cc
@@ -3,37 +3,33 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int N, K;
     cin >> N >> K;
 
-    int extra = 0; //추가해야하는 물병 수
+    //추가로 사야 하는 물병 개수 - 전체에서 누적해서 출력해야 하므로 여기서 초기화
+    int extra = 0;
 
     //반복
     while (true) {
-        //1인 비트 수 검사하기 위한 temp
-        int temp = N + extra;
-
-        //현재 물병 개수
+        //끝 비트 수 검사하기 위한 임시값
+        int temp = extra + N;
+        //현재 물병 개수(1인 비트 수 세기 위한 값)
         int cnt = 0;
-
-        //1인 비트 수 검사. temp 비트를 밀면서 마지막 비트만 검사 반복
-        while (temp>0) {
+        
+        //temp 비트 검사하기
+        while (temp > 0) {
             if (temp & 1) cnt++;
             temp = temp >> 1;
         }
-
-        //종료 조건. k보다 작거나 같으면 종료
+        
+        //while 반복 종료 조건 : cnt K
         if (cnt <= K) {
-            cout << extra;//출력해야 하는 것 : 최종 물병 개수 x!!! 추가로 산 물병 개수
+            //추가한 물병만 출력
+            cout << extra;
             break;
         }
-
-        //(추가)조건 만족 못했을 때는
+        
+        //종료 실패했으면 물병 하나 더 추가
         extra++;
-
     }
 }
